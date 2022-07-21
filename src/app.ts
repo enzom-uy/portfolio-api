f14e913d029d07e2866fbf5d915fd3a6f685f9e1
enzom-uy <enzom.dev@gmail.com>
2022-07-21 20:47:07 -0300

    mmm

--- src/app.ts
+++ src/app.ts
@@ -3,7 +3,9 @@ import cors from 'cors'
 import twitterRoute from './routes/twitter.routes'
 
 const app = express()
-app.listen(process.env.PORT)
+app.listen(process.env.PORT as string)
+
+console.debug(`Server listening on port ${process.env.PORT as string}`)
 app.use(express.json())
 app.use(express.urlencoded({ extended: false }))
 app.use(cors())
