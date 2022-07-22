7ce2b39e93a903c38d737df4c16ef8151239381b
enzom-uy <enzom.dev@gmail.com>
2022-07-21 21:29:56 -0300

    Add "/" route to API
    Add index route to API

--- src/app.ts
+++ src/app.ts
@@ -1,15 +1,17 @@
 import express from 'express'
 import cors from 'cors'
 import twitterRoute from './routes/twitter.routes'
+import indexRoute from './routes/index.routes'
 
 const app = express()
 app.listen(process.env.PORT as string)
-
 console.debug(`Server listening on port ${process.env.PORT as string}`)
+
 app.use(express.json())
 app.use(express.urlencoded({ extended: false }))
 app.use(cors())
 
 app.use(twitterRoute)
+app.use(indexRoute)
 
 export default app
