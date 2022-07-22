b342709586e7c2033b67eb115ab13e436a568f21
enzom-uy <enzom.dev@gmail.com>
2022-07-21 22:10:31 -0300

    Set every config variable as string

--- src/utils/config.ts
+++ src/utils/config.ts
@@ -1,9 +1,9 @@
 // eslint-disable-next-line @typescript-eslint/no-var-requires
 require('dotenv').config()
 
-export const twitterId = process.env.TWITTER_ID
-export const twitterApiKey = process.env.TWITTER_API_KEY
-export const twitterApiKeySecret = process.env.TWITTER_API_KEY_SECRET
-export const twitterBearerToken = process.env.TWITTER_BEARER_TOKEN
-export const twitterFollowings = process.env.TWITTER_FOLLOWINGS
-export const twitterFollowers = process.env.TWITTER_FOLLOWERS
+export const twitterId = process.env.TWITTER_ID as string
+export const twitterApiKey = process.env.TWITTER_API_KEY as string
+export const twitterApiKeySecret = process.env.TWITTER_API_KEY_SECRET as string
+export const twitterBearerToken = process.env.TWITTER_BEARER_TOKEN as string
+export const twitterFollowings = process.env.TWITTER_FOLLOWINGS as string
+export const twitterFollowers = process.env.TWITTER_FOLLOWERS as string
