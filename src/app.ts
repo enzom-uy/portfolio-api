c5d0b5779e73c92beca024d428e405578dd82e62
enzom-uy <enzom.dev@gmail.com>
2022-09-04 21:21:22 -0300

    Fix

--- src/app.ts
+++ src/app.ts
@@ -2,8 +2,6 @@ import express from 'express'
 import cors from 'cors'
 import twitterRoute from './routes/twitter.routes'
 import indexRoute from './routes/index.routes'
-import adminRoute, { loginAdminRoute } from './routes/admin.routes'
-import * as middleware from './middlewares/verifyAuth'
 import './database/db'
 
 const app = express()
@@ -16,8 +14,5 @@ app.use(cors())
 
 app.use(twitterRoute)
 app.use(indexRoute)
-app.use(loginAdminRoute)
-app.use(middleware.verifyAuth)
-app.use(adminRoute)
 
 export default app
